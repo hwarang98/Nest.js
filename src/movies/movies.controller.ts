ea2bf1d1883801a0452b0762200ff17f37bf4d65
LeeHwaRang <snow2271@gmail.com>
2022-07-13 00:01:28 +0900

    url 라우팅

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,12 +9,16 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movies.entities';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private movieService: MoviesService) {}
+
   @Get()
-  getAll(): string {
-    return '모든 영화를 보여줌';
+  getAll(): Movie[] {
+    return this.movieService.getAll();
   }
 
   // nest.js에서 search 부분이 get보다 밑에있으면 search 를 id로 판단하니 위로 올려야됨
@@ -25,30 +29,27 @@ export class MoviesController {
 
   // id가 제일 위에있으면 다른 get들이 작동하지않는다.
   @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return `하나의 영화만 보여줌 영화의 아이디는  ${movieId} 임`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.movieService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData): string {
-    return movieData;
+  create(@Body() movieData) {
+    return this.movieService.create(movieData);
   }
 
   @Delete('/:id')
-  delete(@Param('id') movieDeleteId: string) {
-    return `${movieDeleteId}번 영화 삭제`;
+  remove(@Param('id') movieDeleteId: string) {
+    return this.movieService.deleteOne(movieDeleteId);
   }
 
   @Patch('/:id')
   patchMovies(@Param('id') moviePatchId: string, @Body() patchData) {
-    return {
-      patchMovies: moviePatchId,
-      ...patchData,
-    };
+    return this.movieService.update(moviePatchId, patchData);
   }
 
-  @Put('/:id')
-  putMovies(@Param('id') moviePutId: string) {
-    return `${moviePutId}번 영화 put`;
-  }
+  // @Put('/:id')
+  // putMovies(@Param('id') moviePutId: string) {
+  //   return `${moviePutId}번 영화 put`;
+  // }
 }
