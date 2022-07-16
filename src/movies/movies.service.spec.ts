2bce07490a7c669e3e759db3cb75e2bafbcc3cec
LeeHwaRang <snow2271@gmail.com>
2022-07-16 17:54:14 +0900

    jest 유닛 테스트

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -51,8 +51,50 @@ describe('MoviesService', () => {
         genres: ['test'],
         year: 2000,
       });
-      const result = service.deleteOne(1);
-      expect(result).toEqual(`ID:1 의 영화가 삭제 되었습니다.`);
+      const beforeDelete = service.getAll();
+      service.deleteOne(1);
+      const afterDelete = service.getAll();
+      expect(afterDelete.length).toBeLessThan(beforeDelete.length);
+    });
+    it('404 에러가 나와야함.', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  // describe('create 함수 테스트', () => {
+  //   const beforeCreate = service.getAll().length;
+  //   it('영화가 생성되어야합니다.', () => {
+  //     service.create({
+  //       title: '영화 테스트',
+  //       genres: ['test'],
+  //       year: 2000,
+  //     });
+  //     const afterCreate = service.getAll().length;
+  //     expect(afterCreate).toBeGreaterThan(beforeCreate);
+  //   });
+  // });
+
+  describe('update 함수 테스트', () => {
+    it('영화가 update 되어야함', () => {
+      service.create({
+        title: '영화 테스트',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: '영화 업데이트 테스트' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('영화 업데이트 테스트');
+    });
+    it('NotFoundException가 나와야함', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
     });
   });
 });
